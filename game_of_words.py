57aab842cd830633b9b5dc02f9f0910a97b8d998
Frederic Perez <frederic.perez@about.me>
2017-11-14 18:59:28 +0000

    master :art: Bunch of petty improvements

--- game_of_words.py
+++ game_of_words.py
@@ -8,32 +8,32 @@ def check_dictionary():
     Reads the contents of dictionary.md searching for :shit:,
     or :nine: without part of the speech defined
     """
-    input_file = open("dictionary.md", 'r')
+    filename = 'dictionary.md'
+    input_file = open(filename, 'r')
     num_problems_found = 0
-    num_words_found = 0
 
     for line in input_file:
         if line.find(":shit:") > -1:
             tokens = line.split()
             word = tokens[0]
             part_of_speech = tokens[1]
-            print word, part_of_speech, " <<< FIX tag (use :hammer:) in the dictionary"
+            print word + ' ' + part_of_speech + ' <<< FIX tag (use :hammer:) in the dictionary'
             num_problems_found += 1
         elif line.find(":nine:") > -1:
             tokens = line.split()
             word = tokens[0]
             part_of_speech = tokens[1]
             if part_of_speech.find("_") == -1:
-                print word, part_of_speech, " <<< FIX part of speech in the dictionary!"
+                print word + ' ' + part_of_speech + ' <<< FIX part of speech in the dictionary!'
                 num_problems_found += 1
-            else:
-                print line
-                num_words_found += 1
-
-    print "num_problems_found =", num_problems_found
-    print "num_words_found =", num_words_found
 
     input_file.close()
+    succeeded = bool(num_problems_found == 0)
+    if succeeded:
+        print 'No problems were found in file \'' + filename + '\''
+    else:
+        print 'num_problems_found = ' + str(num_problems_found)
+    return succeeded
 
 def use_random(number):
     "This is a WIP function"
@@ -44,7 +44,5 @@ def use_random(number):
     else:
         print 'Input number =', number, 'is not positive'
 
-print "Script starts..."
 check_dictionary()
 use_random(100)
-print "Script finished--exiting."
