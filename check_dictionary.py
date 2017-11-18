4b8e623f621950cc3648a612bb85cf1f5f858747
Frederic Perez <frederic.perez@about.me>
2017-11-18 10:32:38 +0000

    master :hammer: Refactor creating new function get_headword_and_next_token_index

--- check_dictionary.py
+++ check_dictionary.py
@@ -15,31 +15,32 @@ ENDC = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 
+number_of_composite_headwords = 0
+number_of_lines_with_tag_shit = 0
 number_of_lines_with_too_many_double_spaces = 0
 number_of_lines_with_triple_spaces = 0
-number_of_lines_with_tag_shit = 0
 
 def treat_too_many_double_spaces(line):
     global number_of_lines_with_too_many_double_spaces
     tokens = line.split()
-    word = tokens[0]
+    headword = tokens[0]
     part_of_speech = tokens[1]
-    print word + ' ' + part_of_speech + FAIL + ' <<< Too many double spaces' + ENDC
+    print headword + ' ' + part_of_speech + FAIL + ' <<< Too many double spaces' + ENDC
     number_of_lines_with_too_many_double_spaces += 1
 
 def treat_triple_spaces(line):
     global number_of_lines_with_triple_spaces
     tokens = line.split()
-    word = tokens[0]
-    print word + ' ' + FAIL + ' <<< Triple spaces' + ENDC
+    headword = tokens[0]
+    print headword + ' ' + FAIL + ' <<< Triple spaces' + ENDC
     number_of_lines_with_triple_spaces += 1
 
 def treat_shit_tag(line):
     global number_of_lines_with_tag_shit
     tokens = line.split()
-    word = tokens[0]
+    headword = tokens[0]
     part_of_speech = tokens[1]
-    print word + ' ' + part_of_speech + FAIL + ' <<< :shit: found; use :hammer: instead' + ENDC
+    print headword + ' ' + part_of_speech + FAIL + ' <<< :shit: found; use :hammer: instead' + ENDC
     number_of_lines_with_tag_shit += 1
 
 def line_contains_tag_of_number(line):
@@ -50,17 +51,36 @@ def line_contains_tag_of_number(line):
         or line.find(':five:') > -1 \
         or line.find(':four:') > -1
 
+def get_headword_and_next_token_index(tokens):
+    NUM_TOKENS = len(tokens)
+    headword = tokens[0]
+    headword_completed = headword.count('__') == 2
+    idx = 0
+    while not headword_completed:
+        idx += 1
+        if idx == NUM_TOKENS:
+            headword_completed = True
+        else:
+            headword += ' ' + tokens[idx]
+            if headword.count('__') == 2:
+                headword_completed = True
+    idx += 1
+    if idx > 1:
+        global number_of_composite_headwords
+        number_of_composite_headwords += 1
+        print OKBLUE + headword + GRAY + ' <<< Composite headword #' \
+            + str(number_of_composite_headwords) + ENDC
+    return headword, idx
+
 def check_dictionary():
     """Looking for mistakes in the dictionary
 
     Reads the contents of dictionary.md searching for :shit:,
     or :nine: without part of the speech defined
     """
-    global number_of_lines_with_tag_shit
+    global number_of_lines_missing_part_of_speech
     filename = 'dictionary.md'
     input_file = open(filename, 'r')
-    number_of_composite_words = 0
-    number_of_lines_missing_part_of_speech = 0
     number_of_lines_with_wrong_part_of_speech = 0
 
     for line in input_file:
@@ -72,37 +92,17 @@ def check_dictionary():
             treat_shit_tag(line)
         if line_contains_tag_of_number(line):
             tokens = line.split()
-            NUM_TOKENS = len(tokens)
-            if NUM_TOKENS == 1:
-                number_of_lines_missing_part_of_speech += 1
-                print line + FAIL + ' <<< Missing part of speech #' \
-                    + str(number_of_lines_missing_part_of_speech) + ENDC
-            else:
-                word = tokens[0]
-                word_completed = word.count('__') == 2
-                i = 0
-                while not word_completed:
-                    i += 1
-                    if i == NUM_TOKENS:
-                        word_completed = True
-                    else:
-                        word += ' ' + tokens[i]
-                        if word.count('__') == 2:
-                            word_completed = True
-                if i > 0:
-                    number_of_composite_words += 1
-                    print OKBLUE + word + GRAY + ' <<< Composite word #' \
-                        + str(number_of_composite_words) + ENDC
-                i += 1
-                part_of_speech = tokens[i]
-                if part_of_speech.find('_') == -1:
-                    number_of_lines_with_wrong_part_of_speech += 1
-                    print FAIL + word + ' ' + part_of_speech + ENDC \
-                        + ' <<< Wrong part of speech #' \
-                        + str(number_of_lines_with_wrong_part_of_speech)
+            headword, idx = get_headword_and_next_token_index(tokens)
+            part_of_speech = tokens[idx]
+            if part_of_speech.find('_') == -1:
+                number_of_lines_with_wrong_part_of_speech += 1
+                print FAIL + headword + ' ' + part_of_speech + ENDC \
+                    + ' <<< Wrong part of speech #' \
+                    + str(number_of_lines_with_wrong_part_of_speech)
 
     input_file.close()
-    succeeded = bool(number_of_lines_with_tag_shit + number_of_lines_missing_part_of_speech + number_of_lines_with_wrong_part_of_speech == 0)
+    global number_of_lines_with_tag_shit
+    succeeded = bool(number_of_lines_with_tag_shit + number_of_lines_with_wrong_part_of_speech == 0)
     if succeeded:
         print OKGREEN + 'No problems were found in file \'' + filename + '\'' + ENDC
     else:
@@ -110,7 +110,6 @@ def check_dictionary():
         print '  Lines with triple spaces = ' + str(number_of_lines_with_triple_spaces)
         print '  Lines with too many double spaces = ' + str(number_of_lines_with_too_many_double_spaces)
         print '  Lines with tag :shit: = ' + str(number_of_lines_with_tag_shit)
-        print '  Lines missing part of speech = ' + str(number_of_lines_missing_part_of_speech)
         print '  Lines with wrong part of speech = ' + str(number_of_lines_with_wrong_part_of_speech)
     return succeeded
 
