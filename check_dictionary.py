4a587a0fed6a0bde42f10b59b592ab352b505ff6
Frederic Perez <frederic.perez@about.me>
2017-11-23 18:23:42 +0000

    master :hammer: Refactored functions into new class Checker

--- check_dictionary.py
+++ check_dictionary.py
@@ -14,55 +14,13 @@ ENDC = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 
-number_of_composite_headwords = 0
-number_of_entries_with_invalid_ending = 0
-number_of_entries_with_tag_shit = 0
-number_of_entries_with_too_many_double_spaces = 0
-number_of_entries_with_triple_spaces = 0
-
 def valid_entry_ending(entry):
-    """
-    Self-explanatory
-    """
+    """Self-explanatory"""
     valid = entry.endswith('  ')
     return valid
 
-def treat_invalid_entry_ending(entry):
-    global number_of_entries_with_invalid_ending
-    number_of_entries_with_invalid_ending += 1
-    tokens = entry.split()
-    headword = tokens[0]
-    print headword + ' ' + FAIL + ' <<< Incorrect entry ending #' \
-        + str(number_of_entries_with_invalid_ending) + ENDC
-
-def treat_too_many_double_spaces(entry):
-    global number_of_entries_with_too_many_double_spaces
-    number_of_entries_with_too_many_double_spaces += 1
-    tokens = entry.split()
-    headword = tokens[0]
-    part_of_speech = tokens[1]
-    print headword + ' ' + part_of_speech + FAIL \
-        + ' <<< Too many double spaces #' \
-        + str(number_of_entries_with_too_many_double_spaces) + ENDC
-
-def treat_triple_spaces(entry):
-    global number_of_entries_with_triple_spaces
-    number_of_entries_with_triple_spaces += 1
-    tokens = entry.split()
-    headword = tokens[0]
-    print headword + ' ' + FAIL + ' <<< Triple spaces #' \
-        + str(number_of_entries_with_triple_spaces) + ENDC
-
-def treat_shit_tag(entry):
-    global number_of_entries_with_tag_shit
-    number_of_entries_with_tag_shit += 1
-    tokens = entry.split()
-    headword = tokens[0]
-    part_of_speech = tokens[1]
-    print headword + ' ' + part_of_speech + FAIL \
-        + ' <<< :shit: found; use :hammer: instead' + ENDC
-
-def entry_contains_tag_of_number(entry):
+def entry_has_tag_of_number(entry):
+    """Self-explanatory"""
     return entry.find(':nine:') > -1 \
         or entry.find(':eight:') > -1 \
         or entry.find(':seven:') > -1 \
@@ -70,153 +28,200 @@ def entry_contains_tag_of_number(entry):
         or entry.find(':five:') > -1 \
         or entry.find(':four:') > -1
 
-def entry_contains_tag_of_high_number(entry):
+def entry_has_tag_of_high_number(entry):
+    """Self-explanatory"""
     return entry.find(':nine:') > -1
 
-def entry_contains_tag_hammer(entry):
+def entry_has_tag_hammer(entry):
+    """Self-explanatory"""
     return entry.find(':hammer:') > -1
 
-def get_headword_and_next_token_index(tokens, do_print=False):
-    NUM_TOKENS = len(tokens)
-    headword = tokens[0]
-    headword_completed = headword.count('__') == 2
-    idx = 0
-    while not headword_completed:
-        idx += 1
-        if idx == NUM_TOKENS:
-            headword_completed = True
-        else:
-            headword += ' ' + tokens[idx]
-            if headword.count('__') == 2:
-                headword_completed = True
-    idx += 1
-    if idx > 1:
-        global number_of_composite_headwords
-        number_of_composite_headwords += 1
-        if do_print:
-            print OKBLUE + headword + GRAY + ' <<< Composite headword #' \
-                + str(number_of_composite_headwords) + ENDC
-    return headword, idx
-
 def print_colored(label, number):
+    """Self-explanatory"""
     message = label + ' = ' + str(number)
     if number == 0:
         print OKGREEN + message + ENDC
     else:
         print FAIL + message + ENDC
 
-def check_dictionary_entries():
-    """Looking for mistakes in the entries of the dictionary
+class Checker(object):
+    """The class Checker encapsulates all functionalities to check the dictionary"""
+    def __init__(self, filename):
+        self.filename = filename
+        self.num_composite_headwords = 0
+        self.num_entries_with_invalid_ending = 0
+        self.num_entries_with_tag_shit = 0
+        self.num_entries_with_too_many_double_spaces = 0
+        self.num_entries_with_triple_spaces = 0
 
-    Reads the contents of dictionary.md searching for :shit:,
-    or :nine: without part of the speech defined
-    """
-    filename = 'dictionary.md'
-    input_file = open(filename, 'r')
-    number_of_entries_with_wrong_part_of_speech = 0
-
-    for line in input_file:
-        entry = line.replace('\n', '')
-        if not valid_entry_ending(entry):
-            treat_invalid_entry_ending(entry)
-        if entry.count('  ') > 2:
-            treat_too_many_double_spaces(entry)
-        if entry.count('   ') > 0:
-            treat_triple_spaces(entry)
-        if entry.find(':shit:') > -1:
-            treat_shit_tag(entry)
-        if entry_contains_tag_of_number(entry):
-            tokens = entry.split()
-            do_print = True
-            headword, idx = get_headword_and_next_token_index(tokens, do_print)
-            part_of_speech = tokens[idx]
-            if part_of_speech.find('_') == -1:
-                number_of_entries_with_wrong_part_of_speech += 1
-                print FAIL + headword + ' ' + BOLD + part_of_speech + ENDC \
-                    + ' <<< Wrong part of speech #' \
-                    + str(number_of_entries_with_wrong_part_of_speech)
-
-    input_file.close()
-    global number_of_entries_with_invalid_ending
-    global number_of_entries_with_tag_shit
-    succeeded = bool(number_of_entries_with_invalid_ending \
-        + number_of_entries_with_tag_shit \
-        + number_of_entries_with_wrong_part_of_speech == 0)
-    if succeeded:
-        print OKGREEN + 'No entries-related problems were found in file \'' + filename + '\'' + ENDC
-    else:
-        print '\nSummary of issues found'
-        print '-----------------------'
-        print_colored('Entries with invalid ending', number_of_entries_with_invalid_ending)
-        print_colored('Entries with triple spaces', number_of_entries_with_triple_spaces)
-        print_colored('Entries with too many double spaces', \
-            number_of_entries_with_too_many_double_spaces)
-        print_colored('Entries with tag :shit:', number_of_entries_with_tag_shit)
-        print_colored('Entries with wrong part of speech', \
-            number_of_entries_with_wrong_part_of_speech)
-    print
-    return succeeded
-
-def check_dictionary_duplicated_headwords():
-    filename = 'dictionary.md'
-    input_file = open(filename, 'r')
-
-    repeated = {}
-    dictionary = {}
-
-    for line in input_file:
-        entry = line.replace('\n', '')
+    def treat_invalid_entry_ending(self, entry):
+        """Self-explanatory"""
+        self.num_entries_with_invalid_ending += 1
         tokens = entry.split()
-        do_print = False
-        headword, idx = get_headword_and_next_token_index(tokens, do_print)
-        if dictionary.get(headword) == 1:
-            repeated[headword] = 1
-        dictionary[headword] = 1
+        headword = tokens[0]
+        print headword + ' ' + FAIL + ' <<< Incorrect entry ending #' \
+            + str(self.num_entries_with_invalid_ending) + ENDC
 
-    input_file.close()
+    def treat_too_many_double_spaces(self, entry):
+        """Self-explanatory"""
+        self.num_entries_with_too_many_double_spaces += 1
+        tokens = entry.split()
+        headword = tokens[0]
+        part_of_speech = tokens[1]
+        print headword + ' ' + part_of_speech + FAIL \
+            + ' <<< Too many double spaces #' \
+            + str(self.num_entries_with_too_many_double_spaces) + ENDC
 
-    N = len(repeated)
-    if N > 0:
-        repeated_sorted = sorted(repeated)
-        print 'Found ' + str(N) + ' duplicated headwords, the very first being:'
-        i = 0
-        for headword in repeated_sorted:
-            print '  ' + FAIL + headword + ENDC
-            i += 1
-            if i == 10:
-                break
-    else:
-        print OKGREEN + 'No duplicated headwords were found' + ENDC
+    def treat_triple_spaces(self, entry):
+        """Self-explanatory"""
+        self.num_entries_with_triple_spaces += 1
+        tokens = entry.split()
+        headword = tokens[0]
+        print headword + ' ' + FAIL + ' <<< Triple spaces #' \
+            + str(self.num_entries_with_triple_spaces) + ENDC
 
-def check_dictionary_undefined_high_frequency_keywords():
-    filename = 'dictionary.md'
-    input_file = open(filename, 'r')
+    def treat_shit_tag(self, entry):
+        """Self-explanatory"""
+        self.num_entries_with_tag_shit += 1
+        tokens = entry.split()
+        headword = tokens[0]
+        part_of_speech = tokens[1]
+        print headword + ' ' + part_of_speech + FAIL \
+            + ' <<< :shit: found; use :hammer: instead' + ENDC
 
-    undefined = {}
+    def get_headword_and_next_token_idx(self, tokens, do_print=False):
+        """Self-explanatory"""
+        num_tokens = len(tokens)
+        headword = tokens[0]
+        headword_completed = headword.count('__') == 2
+        idx = 0
+        while not headword_completed:
+            idx += 1
+            if idx == num_tokens:
+                headword_completed = True
+            else:
+                headword += ' ' + tokens[idx]
+                if headword.count('__') == 2:
+                    headword_completed = True
+        idx += 1
+        if idx > 1:
+            self.num_composite_headwords += 1
+            if do_print:
+                print OKBLUE + headword + GRAY + ' <<< Composite headword #' \
+                    + str(self.num_composite_headwords) + ENDC
+        return headword, idx
 
-    for line in input_file:
-        entry = line.replace('\n', '')
-        if entry_contains_tag_of_high_number(entry):
-            if entry_contains_tag_hammer(entry):
+    def check_dictionary_entries(self):
+        """Looking for mistakes in the entries of the dictionary
+
+        Reads the contents of self.dictionary searching for :shit:,
+        or :nine: without part of the speech defined
+        """
+        input_file = open(self.filename, 'r')
+        num_entries_with_wrong_part_of_speech = 0
+
+        for line in input_file:
+            entry = line.replace('\n', '')
+            if not valid_entry_ending(entry):
+                self.treat_invalid_entry_ending(entry)
+            if entry.count('  ') > 2:
+                self.treat_too_many_double_spaces(entry)
+            if entry.count('   ') > 0:
+                self.treat_triple_spaces(entry)
+            if entry.find(':shit:') > -1:
+                self.treat_shit_tag(entry)
+            if entry_has_tag_of_number(entry):
                 tokens = entry.split()
-                do_print = False
-                headword, idx = get_headword_and_next_token_index(tokens, do_print)
-                undefined[headword] = 1
+                do_print = True
+                headword, idx = self.get_headword_and_next_token_idx(tokens, do_print)
+                part_of_speech = tokens[idx]
+                if part_of_speech.find('_') == -1:
+                    num_entries_with_wrong_part_of_speech += 1
+                    print FAIL + headword + ' ' + BOLD + part_of_speech + ENDC \
+                        + ' <<< Wrong part of speech #' \
+                        + str(num_entries_with_wrong_part_of_speech)
 
-    input_file.close()
+        input_file.close()
+        succeeded = bool(self.num_entries_with_invalid_ending \
+            + self.num_entries_with_tag_shit \
+            + num_entries_with_wrong_part_of_speech == 0)
+        if succeeded:
+            print OKGREEN + 'No entries-related problems were found in file \'' \
+                + self.filename + '\'' + ENDC
+        else:
+            print '\nSummary of issues found'
+            print '-----------------------'
+            print_colored('Entries with invalid ending', self.num_entries_with_invalid_ending)
+            print_colored('Entries with triple spaces', self.num_entries_with_triple_spaces)
+            print_colored('Entries with too many double spaces', \
+                self.num_entries_with_too_many_double_spaces)
+            print_colored('Entries with tag :shit:', self.num_entries_with_tag_shit)
+            print_colored('Entries with wrong part of speech', \
+                num_entries_with_wrong_part_of_speech)
+        print
+        return succeeded
 
-    N = len(undefined)
-    if N > 0:
-        undefined_sorted = sorted(undefined)
-        print 'Found ' + str(N) + ' undefined high frequency headwords, the very first being:'
-        i = 0
-        for headword in undefined_sorted:
-            print '  ' + FAIL + headword + ENDC
-            i += 1
-            if i == 10:
-                break
-    else:
-        print OKGREEN + 'No undefined high frequency headwords were found' + ENDC
+    def check_dictionary_duplicated_headwords(self):
+        """Self-explanatory"""
+        input_file = open(self.filename, 'r')
+
+        repeated = {}
+        dictionary = {}
+
+        for line in input_file:
+            entry = line.replace('\n', '')
+            tokens = entry.split()
+            do_print = False
+            headword, idx = self.get_headword_and_next_token_idx(tokens, do_print)
+            if dictionary.get(headword) == 1:
+                repeated[headword] = 1
+            dictionary[headword] = 1
+
+        input_file.close()
+
+        size = len(repeated)
+        if size > 0:
+            repeated_sorted = sorted(repeated)
+            print 'Found ' + str(size) + ' duplicated headwords, the very first being:'
+            i = 0
+            for headword in repeated_sorted:
+                print '  ' + FAIL + headword + ENDC
+                i += 1
+                if i == 10:
+                    break
+        else:
+            print OKGREEN + 'No duplicated headwords were found' + ENDC
+
+    def check_dictionary_undefined_high_frequency_keywords(self):
+        """Self-explanatory"""
+        input_file = open(self.filename, 'r')
+
+        undefined = {}
+
+        for line in input_file:
+            entry = line.replace('\n', '')
+            if entry_has_tag_of_high_number(entry):
+                if entry_has_tag_hammer(entry):
+                    tokens = entry.split()
+                    do_print = False
+                    headword, idx = self.get_headword_and_next_token_idx(tokens, do_print)
+                    undefined[headword] = 1
+
+        input_file.close()
+
+        size = len(undefined)
+        if size > 0:
+            undefined_sorted = sorted(undefined)
+            print 'Found ' + str(size) \
+                + ' undefined high frequency headwords, the very first being:'
+            i = 0
+            for headword in undefined_sorted:
+                print '  ' + FAIL + headword + ENDC
+                i += 1
+                if i == 10:
+                    break
+        else:
+            print OKGREEN + 'No undefined high frequency headwords were found' + ENDC
 
 def use_random(number):
     "This is a WIP function"
@@ -227,9 +232,11 @@ def use_random(number):
     else:
         print 'Input number =', number, 'is not positive'
 
-check_dictionary_entries()
-check_dictionary_duplicated_headwords()
+CHECKER = Checker('dictionary.md')
+CHECKER.check_dictionary_entries()
+CHECKER.check_dictionary_duplicated_headwords()
 print
-check_dictionary_undefined_high_frequency_keywords()
+CHECKER.check_dictionary_undefined_high_frequency_keywords()
 print
+
 use_random(100)
