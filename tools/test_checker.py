012f22df988238b1a616df8a2650a9916ea0dc42
Frederic Perez <frederic.perez@gmx.us>
2019-01-28 15:57:33 +0100

    master :books: First baby steps for TDD

--- tools/test_checker.py
+++ tools/test_checker.py
@@ -1,15 +1,16 @@
 """
 Run the tests by executing:
-$ python -m unittest -v test_dictionary
+$ python -m unittest -v test_checker
 """
 
 import unittest
-from dictionary import Dictionary
+from dictionary import Checker
 
 class Test_Dictionary(unittest.TestCase):
 
     def test_GIVEN_an_empty_dictionary_WHEN_calling_xyz_THEN_result_must_be_equal_to_xyz(self):
-        DICTIONARY = Dictionary()
+        DICTIONARY = '../data/dictionary.md'
+        CHECKER = Checker(DICTIONARY)¡
         self.assertEqual(2, 1)
 
 if __name__ == '__main__':
