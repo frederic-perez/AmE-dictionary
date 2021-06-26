9d329af00d72e94e41c1883cd18288b0fa9875ef
Frederic Perez <frederic.perez@gmx.us>
2021-06-27 00:13:33 +0200

    ♻️ Side effects after splitting `top.md`

--- tools/checker.py
+++ tools/checker.py
@@ -591,7 +591,7 @@ def main(progname, argv):
     if num_arguments > 0:
         print(OKBLUE + progname + ': Argument list: ' + str(argv) + ENDC)
 
-    VALID_ARGUMENTS = [ "todo-idioms", "todo-main", "dictionary", "idioms" ]
+    VALID_ARGUMENTS = [ "dictionary", "idioms", "todo-idioms", "todo-main", "top-dictionary", "top-idioms" ]
     if num_arguments > 2:
         print(RED + "Too many arguments. Aborting..." + ENDC)
         usageAndAbort(progname, VALID_ARGUMENTS)
@@ -605,7 +605,7 @@ def main(progname, argv):
         print(OKBLUE + progname + ': Argument list after adding default arguments: ' + str(argv) + ENDC)
 
     for arg in argv:
-        do_check_parts_of_speech = arg != 'idioms' and arg != 'todo-idioms'
+        do_check_parts_of_speech = arg != 'idioms' and arg != 'todo-idioms' and arg != 'top-idioms'
         check(arg, do_check_parts_of_speech)
 
 if __name__ == '__main__':
